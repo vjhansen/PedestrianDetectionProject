9ed3ee683ae5d451d04f27e584129896455c3fe6
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-01-21 20:26:57 +0100

    Update klassifisering_KNN.py

--- klassifisering_KNN.py
+++ klassifisering_KNN.py
@@ -1,1 +1,13 @@
 import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+#dataset = ---
+(x_train, y_train),(x_test, y_test) = dataset.load_data()
+
+
+# lineær modell: input --> hidden #1 --> hidden #2 --> output
+model = Sequential()
+model.add(Dense(32, input_shape=(500,)))
+
+
