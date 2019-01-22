a307ba4bfea94f1e72e9ed028d3331ce0e04d9be
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-01-22 18:03:35 +0100

    Update augment-dataset.py

--- augment-dataset.py
+++ augment-dataset.py
@@ -4,13 +4,13 @@
 import Augmentor
 import os
 
-src = os.getcwd()+"/augment_datasett/resize_px/" 
+src = os.getcwd()+"/datasett/" 
 
 # dst-folder blir opprettet som 'output' i src-folder
 
 p = Augmentor.Pipeline(src)
 
-p.skew(probability=0.7, magnitude=1.0)
-p.rotate(probability=1, max_left_rotation=5, max_right_rotation=5)
-p.flip_left_right(probability=0.5)
-p.sample(1000)
+#p.skew(probability=0.7, magnitude=1.0)
+#p.rotate(probability=1, max_left_rotation=5, max_right_rotation=5)
+p.flip_left_right(probability=1)
+p.sample(1000) # filer i folder
