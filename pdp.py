6d000cc56ca84e473a9acab2fa86ae1224580128
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-01-27 22:27:00 +0100

    Update pdp.py

--- pdp.py
+++ pdp.py
@@ -1,64 +1,40 @@
 #models/research/object_detection
 
 import numpy as np
-import os
-import six.moves.urllib as urllib
-import sys
-import tarfile
+import os, sys, cv2
 import tensorflow as tf
-import zipfile
+import zipfile, tarfile
 
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
 from PIL import Image
 
-import cv2
-cap = cv2.VideoCapture(0)
+#cap = cv2.VideoCapture(0) # webcam: prøv (-1,0,1)
+cap = cv2.VideoCapture('vid2.mp4') # teste video
 
-# This is needed since the notebook is stored in the object_detection folder.
 sys.path.append("..")
 
-
-# ## Object detection imports
 # Here are the imports from the object detection module.
 from utils import label_map_util
-
 from utils import visualization_utils as vis_util
 
 
-# # Model preparation 
-# ## Variables
+# Model preparation 
 # Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
 
-# What model to download.
-MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-MODEL_FILE = MODEL_NAME + '.tar.gz'
-DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+MODEL_NAME = 'pdp-alpha-v2'
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
 # List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
+PATH_TO_LABELS = os.path.join('training', 'training_ob-det.pbtxt')
 
 # vi har bare 1 class = person
-NUM_CLASSES = 90
-
-# Download Model
-
-opener = urllib.request.URLopener()
-opener.retrieve(DOWNLOAD_BASE + MODEL_FILE, MODEL_FILE)
-tar_file = tarfile.open(MODEL_FILE)
-for file in tar_file.getmembers():
-  file_name = os.path.basename(file.name)
-  if 'frozen_inference_graph.pb' in file_name:
-    tar_file.extract(file, os.getcwd())
-
+NUM_CLASSES = 1
 
 # -- Load a (frozen) Tensorflow model into memory --
-
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
@@ -77,24 +53,7 @@ label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
-# ## Helper code
-def load_image_into_numpy_array(image):
-  (im_width, im_height) = image.size
-  return np.array(image.getdata()).reshape(
-      (im_height, im_width, 3)).astype(np.uint8)
-
-
 # -- Detection -- 
-# For the sake of simplicity we will use only 2 images:
-# image1.jpg
-# image2.jpg
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'test_images'
-TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
-
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     while True:
@@ -125,9 +84,11 @@ with detection_graph.as_default():
           np.squeeze(scores),
           category_index,
           use_normalized_coordinates=True,
+          min_score_thresh = .5, #50%
           line_thickness=5)
 
-      cv2.imshow('PDP', cv2.resize(image_np, (800,600)))
-      if cv2.waitKey(25) & 0xFF == ord('q'):
+      cv2.imshow('PDP', cv2.resize(image_np, (720,576)))
+      if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+cap.release()
