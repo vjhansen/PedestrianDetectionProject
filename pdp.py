0ce783421b3888d1862fac1a6e6912023470707e
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-01-31 14:37:48 +0100

    Update pdp.py

--- pdp.py
+++ pdp.py
@@ -1,127 +1,129 @@
-# 30.01
-# python3 pdp.py
+# 31.01, victor
+# python 3
 
-import numpy as np
-import sys, os, cv2, time
+import numpy
+import sys
+import os
+import cv2
 import tensorflow as tf
-import zipfile, tarfile
+import serial # - kommunikasjon med Arduino
 
 from collections import defaultdict
 from io import StringIO
 from matplotlib import pyplot as plt
-from PIL import Image
-
-#cap = cv2.VideoCapture(0) # webcam: prøv (-1,0,1)
-cap = cv2.VideoCapture('vid2.mp4') # teste video
-time.sleep(3.0)
-
-sys.path.append("..")
-# Import fra object detection module.
 from utils import label_map_util
 from utils import visualization_utils as vis_util
 
-# Model preparation 
-MODEL_NAME = 'pdp-alpha-v2'
+#cap = cv2.VideoCapture(0) # - usbkamera/webcam: forsøk (-1), (0) eller (1)
+cap = cv2.VideoCapture('vid3.mp4') # - teste video
 
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
+#sys.path.append("..")
 
-# List of the strings that is used to add correct label for each box.
-PATH_TO_LABELS = os.path.join('training', 'training_ob-det.pbtxt')
+modell = 'pdp-alpha-v2'
 
-NUM_CLASSES = 1
+# - Path to frozen detection graph. This is the actual model that is used for the object detection.
+PATH_TO_CKPT = modell + '/frozen_inference_graph.pb'
 
-# -- Load a (frozen) Tensorflow model into memory --
+# - List of the strings that is used to add correct label for each box.
+label_path = os.path.join('training', 'training_ob-det.pbtxt')
 
+# - Laster inn en(frozen) Tensorflow model into memory
 detection_graph = tf.Graph()
 with detection_graph.as_default():
   od_graph_def = tf.GraphDef()
   with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
     serialized_graph = fid.read()
     od_graph_def.ParseFromString(serialized_graph)
-    tf.import_graph_def(od_graph_def, name='')
+    tf.import_graph_def(od_graph_def, name = '')
 
-
-# -- Loading label map --
-# Label maps map indices to category names, so that when our convolution network predicts `5`, 
-# we know that this corresponds to `airplane`.  
+# - Load label map
+# Label maps map indices to category names, so that when our CNN predicts "1", 
+# vet vi at dette er en "person". 
 # Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 
-label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+label_map = label_map_util.load_labelmap(label_path)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes = 1, use_display_name = True)
 category_index = label_map_util.create_category_index(categories)
 
+# - Tegner centroid-sirkel, radius = 10, farge = rød
 def circle_draw(frame, x,y):
   cv2.circle(frame, (x,y), 10, (0,0,255), -1)
 
+#ser = serial.Serial('/dev/tty.usbserial', 9600) # - Åpne serialport for komm. med Arduino
+
 with detection_graph.as_default():
-  with tf.Session(graph=detection_graph) as sess:
+  with tf.Session(graph = detection_graph) as sess:
     while True:
+
+	  # - leser videokilde    
       ret, frame = cap.read()
+
+      # - størrelse på display-vindu
       im_w = 720
       im_h = 576
       cv2.resize(frame, (im_w, im_h))
-      stime = time.time()
       
-      # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
-      frame_expanded = np.expand_dims(frame, axis=0)
+      # - Expand dimensions since the model expects images to have shape: [1, None, None, 3]
+      frame_expanded = numpy.expand_dims(frame, axis = 0)
       image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
       
-      # Each box represents a part of the image where a particular object was detected.
+      # - Each box represents a part of the image where a particular object was detected.
       boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
       
-      # Each score represent how level of confidence for each of the objects.
-      # Score is shown on the result image, together with the class label.
+      # - Each score represent how level of confidence for each of the objects.
+      # - Score is shown on the result image, together with the class label.
       scores = detection_graph.get_tensor_by_name('detection_scores:0')
       classes = detection_graph.get_tensor_by_name('detection_classes:0')
       num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
-      #Actual detection.
+      # - Actual detection.
       (boxes, scores, classes, num_detections) = sess.run(
           [boxes, scores, classes, num_detections],
-          feed_dict={image_tensor: frame_expanded})
-      
-      score_thresh = 0.6
-      max_bbx = 2
-      
-      # Visualization of the results of a detection.
+          feed_dict = {image_tensor: frame_expanded})
+  
+      score_thresh = 0.6 # - nedre grense for prediction-score
+      max_bbx = 2 # - maks. antall bounding boxes som skal tegnes
+
+      # - Visualisering av detekteringen
+      # - numpy.squeeze fjerner 1 dimensjonale oppføringer fra formen på input-array
       vis_util.visualize_boxes_and_labels_on_image_array(
           frame,
-          np.squeeze(boxes),
-          np.squeeze(classes).astype(np.int32),
-          np.squeeze(scores),
+          numpy.squeeze(boxes),
+          numpy.squeeze(classes).astype(numpy.int32),
+          numpy.squeeze(scores),
           category_index,
-          use_normalized_coordinates=True,
           max_boxes_to_draw = max_bbx,
           min_score_thresh = score_thresh,
           line_thickness = 4)
       
-      # getting normalized coordinates for bounding box
-      # ymin og ymax bytter plass (pga. koordinatsystem i pyplot)
-      ymax = (boxes[0][0][0]) 
+      # - Normaliserte bbx-koordinater (i forhold til størrelsen på input-frame)
+      # - koord. er på formen [y_min, x_min, y_max, x_max]
+      ymin = (boxes[0][0][0]) 
       xmin = (boxes[0][0][1])
-      ymin = (boxes[0][0][2])
+      ymax = (boxes[0][0][2])
       xmax = (boxes[0][0][3])
 
-      cols = (frame.shape[1])
-      rows = (frame.shape[0])
+      # - W (bredde) og H (høyde) endres med oppløsningen på film/kamera
+      # - ex. webcam på mac gir 1920x1080
+      W = (frame.shape[1]) # - horisontal
+      H = (frame.shape[0]) # - vertikal
+      #print(W,H)
       
-      # sentrum av rektangel
-      xCenter = int((xmax + xmin)*cols / 2.0)
-      yCenter = int((ymax + ymin)*rows / 2.0)
+      # - Sentrum av bounding box
+      xCenter = int((xmax + xmin)*W / 2.0)
+      yCenter = int((ymax + ymin)*H / 2.0)
 
-      (left, right, top, bottom) = (xmin*im_w, xmax*im_w, ymin*im_h, ymax*im_h)
+      # (left, right, top, bottom) = (xmin*W, xmax*W, ymax*H, ymin*H)
       
-      # openCV stuff....
-      # print('FPS {:.1f}'.format(1/(time.time()-stime)))
-      
+      # - Tegner sirkel i sentrum av bounding box med høyeste score
       for i in range(min(max_bbx, boxes.shape[0])):
-        if np.squeeze(scores)[i] > score_thresh:
+        if numpy.squeeze(scores)[i] > score_thresh:
           circle_draw(frame, xCenter, yCenter)
-          print('target acquired')
-      
-      cv2.imshow('PDP', cv2.resize(frame, (im_w, im_h)))
-      if cv2.waitKey(1) & 0xFF == ord('q'):
+          print(str(xCenter)+'x'+str(yCenter))
+          #ser.write(xCenter,yCenter) # - skriver koord. til Arduino
+
+      cv2.imshow('', cv2.resize(frame, (im_w, im_h))) # - viser detektering ++ i sanntid
+      if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 cv2.destroyAllWindows()
 cap.release()
