eb058c14aeb64feb235573a9e83a8dfea8be1f82
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-02-11 19:03:39 +0100

    Update servo_pan_tilt_arduino.ino

--- servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
@@ -1,20 +1,30 @@
+// siste edit: 11.02.19
+
+// Arduino-kode for styring av servoer
+
 #include <Servo.h>
+// se denne https://www.sparkfun.com/tutorials/304
+// kan hjelpe oss med tracking.
 
-
+Servo servo_pan, servo_tilt;
+int pos = 0;
 
 void setup() {
-
-servo_pan = Servo.attach(9);    // - PIN 9
-servo_tilt = Servo.attach(10);  // - PIN 10
-
+servo_pan.attach(3);    // - PIN 3
+servo_tilt.attach(2);   // - PIN 2
 }
 
 void loop() {
 // - 0 til 180 grader
 // - 1 grad pr. step
-for (int pos = 0; pos <= 180; pos += 1){
-  servo_pan.write(pos);
-  delay(15);
-}
-
+  for (int pos = 0; pos <= 180; pos += 1){
+    servo_pan.write(pos);
+    servo_pan.write(pos);
+    delay(50);
+  }
+  for (pos = 180; pos >= 0; pos -=1){
+    servo_pan.write(pos);
+    servo_tilt.write(pos);
+    delay(50);
+  }
 }
