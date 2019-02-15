86be759ab1c8533930a3c62b658d2c28804d7326
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-02-15 23:53:26 +0100

    Update pdp.py

--- pdp.py
+++ pdp.py
@@ -1,11 +1,11 @@
 # PDP - Bachelor 19, AUT, UiT
-# update: 31.01, victor
-# python 3
+# update: 15.02, victor
 
-# se denne: https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
+# https://www.pyimagesearch.com/2015/12/21/increasing-webcam-fps-with-python-and-opencv/
 
 import numpy
 import sys
+import time
 import os
 import cv2
 import tensorflow as tf
@@ -18,10 +18,10 @@ from utils import label_map_util
 from utils import visualization_utils as vis_util
 
 #cap = cv2.VideoCapture(0) # - usbkamera/webcam: forsøk (-1), (0) eller (1)
-cap = cv2.VideoCapture('vid3.mp4') # - teste video
+cap = cv2.VideoCapture('vid_town.avi') # - teste video
 
 # - laster inn ferdigtrent modell
-modell = 'pdp-alpha-v2'
+modell = 'pdp-alpha-v1'
 
 # - Frozen detection graph. Dette er modellen som brukes for detekteringen.
 PATH_TO_CKPT = modell + '/frozen_inference_graph.pb'
@@ -44,21 +44,19 @@ label_map = label_map_util.load_labelmap(label_path)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes = 1, use_display_name = True)
 category_index = label_map_util.create_category_index(categories)
 
-# - Tegner centroid-sirkel, radius = 10, farge = rød
-def circle_draw(frame, x,y):
-  cv2.circle(frame, (x,y), 10, (0,0,255), -1)
-
 #ser = serial.Serial('/dev/tty.usbserial', 9600) # - Åpne serialport for komm. med Arduino
 
 with detection_graph.as_default():
   with tf.Session(graph = detection_graph) as sess:
     while True:
+      # - leser videokilde    
       ret, frame = cap.read()
+      timer = cv2.getTickCount()
       # - størrelse på display-vindu
       im_w = 720
       im_h = 576
       cv2.resize(frame, (im_w, im_h))
-    
+      
       # - bruker expand_dims for å endre formen på frame fra (1080, 1920, 3) til (1, 1080, 1920, 3)
       frame_expanded = numpy.expand_dims(frame, axis = 0)
       image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
@@ -69,28 +67,32 @@ with detection_graph.as_default():
       # - Each score represent how level of confidence for each of the objects.
       # - Score is shown on the result image, together with the class label.
       scores = detection_graph.get_tensor_by_name('detection_scores:0')
-      classes = detection_graph.get_tensor_by_name('detection_classes:0')
-      num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      #classes = detection_graph.get_tensor_by_name('detection_classes:0')
+      #num_detections = detection_graph.get_tensor_by_name('num_detections:0')
 
       # - Actual detection.
-      (boxes, scores, classes, num_detections) = sess.run(
-          [boxes, scores, classes, num_detections],
+      #(boxes, scores, classes, num_detections) = sess.run(
+        #  [boxes, scores, classes, num_detections],
+        #  feed_dict = {image_tensor: frame_expanded})
+
+      (boxes, scores) = sess.run(
+          [boxes, scores],
           feed_dict = {image_tensor: frame_expanded})
   
-      score_thresh = 0.6 # - nedre grense for prediction-score
-      max_bbx = 2 # - maks. antall bounding boxes som skal tegnes
+      score_thresh = 0.5 # - nedre grense for prediction-score
+      max_bbx = 3 # - maks. antall bounding boxes som skal tegnes
 
       # - Visualisering av detekteringen
       # - numpy.squeeze fjerner 1 dimensjonale oppføringer fra formen på input-array
-      vis_util.visualize_boxes_and_labels_on_image_array(
-          frame,
-          numpy.squeeze(boxes),
-          numpy.squeeze(classes).astype(numpy.int32),
-          numpy.squeeze(scores),
-          category_index,
-          max_boxes_to_draw = max_bbx,
-          min_score_thresh = score_thresh,
-          line_thickness = 4)
+      ##vis_util.visualize_boxes_and_labels_on_image_array(
+         ## frame,
+         ## numpy.squeeze(boxes),
+         ## numpy.squeeze(classes).astype(numpy.int),
+         ## numpy.squeeze(scores),
+         ## category_index,
+         ## max_boxes_to_draw = max_bbx,
+         ## min_score_thresh = score_thresh,
+         ## line_thickness = 10)
       
       # - Normaliserte bbx-koordinater (i forhold til størrelsen på input-frame)
       # - koord. er på formen [y_min, x_min, y_max, x_max]
@@ -99,7 +101,7 @@ with detection_graph.as_default():
       ymax = (boxes[0][0][2])
       xmax = (boxes[0][0][3])
 
-      # - W (bredde) og H (høyde) endres med oppløsningen på input
+      # - W (bredde) og H (høyde) endres med oppløsningen på film/kamera
       # - ex. webcam på mac gir 1920x1080
       W = (frame.shape[1]) # - horisontal
       H = (frame.shape[0]) # - vertikal
@@ -109,17 +111,24 @@ with detection_graph.as_default():
       xCenter = int((xmax + xmin)*W / 2.0)
       yCenter = int((ymax + ymin)*H / 2.0)
 
+      n_ymin = int(ymin*H)
+      n_xmin = int(xmin*W)
+      n_ymax = int(ymax*H)
+      n_xmax = int(xmax*W)
       # (left, right, top, bottom) = (xmin*W, xmax*W, ymax*H, ymin*H)
       
       # - Tegner sirkel i sentrum av bounding box med høyeste score
       for i in range(min(max_bbx, boxes.shape[0])):
         if numpy.squeeze(scores)[i] > score_thresh:
-          circle_draw(frame, xCenter, yCenter)
-          print(str(xCenter)+'x'+str(yCenter))
+          cv2.circle(frame, (xCenter,yCenter), 10, (0,0,255), -1)
+          #print(str(xCenter)+'x'+str(yCenter))
+          #cv2.rectangle(frame, (n_xmin, n_ymin), (n_xmax, n_ymax), (0,0,255), 10)
           #ser.write(xCenter,yCenter) # - skriver koord. til Arduino
 
-      cv2.imshow('', cv2.resize(frame, (im_w, im_h))) # - viser detektering ++ i sanntid
-      if cv2.waitKey(10) & 0xFF == ord('q'):
-	cv2.destroyAllWindows()
-	cap.release()
+      fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+      cv2.putText(frame, "FPS: " +str(int(fps)), (100,50), cv2.FONT_HERSHEY_SIMPLEX, 1.75, (50,170,50), 2)
+      cv2.imshow('Fotgjengerdetektering', cv2.resize(frame, (im_w, im_h))) # - viser detektering ++ i sanntid
+      if cv2.waitKey(1) & 0xFF == ord('q'):
+        cv2.destroyAllWindows()
+        cap.release()
         break
