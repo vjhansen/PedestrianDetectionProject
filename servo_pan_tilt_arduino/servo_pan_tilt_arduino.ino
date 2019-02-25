33b33bea6316223ac37bfb7d4c40f41ce3303e90
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-02-25 17:30:09 +0100

    Update servo_pan_tilt_arduino.ino

--- servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
@@ -1,38 +1,32 @@
-// Siste edit: 21.02.19
-// Styring av servoer
-
-
 #include <Servo.h>
-// se denne https://www.sparkfun.com/tutorials/304
-// kan hjelpe oss med tracking.
 
-#define PAN_PIN 3
-#define TILT_PIN 2
-
-Servo SERVO_PAN, SERVO_TILT;
-int pos = 0;
+Servo servo_pan, servo_tilt;
+int data_x = 0;
+int data_y = 0;
+int data[1];
 
 void setup() {
-SERVO_PAN.attach(PAN_PIN);    // - PIN 3
-SERVO_TILT.attach(TILT_PIN);   // - PIN 2
-  
-SERVO_PAN.write(0);   //- inital pos
-SERVO_TILT.write(0);
-Serial.begin(9600);
+    Serial.begin(9600);
+    servo_pan.attach(10);     // - PIN 9
+    servo_tilt.attach(9);     // - PIN 10
+
+    // - startpos
+    servo_pan.write(90); 
+    servo_tilt.write(40);
 }
 
 void loop() {
-  readSerial();
-// - 0 til 180 grader
-// - 1 grad pr. step
-  for (int pos = 0; pos <= 180; pos += 1){
-    servo_pan.write(pos);
-    servo_pan.write(pos);
-    delay(50);
-  }
-  for (pos = 180; pos >= 0; pos -=1){
-    servo_pan.write(pos);
-    servo_tilt.write(pos);
-    delay(50);
-  }
+  while (Serial.available() >= 2) {
+    for (int i = 0; i < 2; i++) {
+      data[i] = Serial.read();
+      }
+      
+      servo_pan.write(data[0]);
+      delay(20);
+      servo_tilt.write(data[1]);
+      delay(20);
+    
+      Serial.println(data[0]);
+      Serial.println(data[1]);
+    }
 }
