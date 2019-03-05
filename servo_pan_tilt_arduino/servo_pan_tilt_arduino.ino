f53195f870fa7121a1c3b97896694b044f2a7918
Victor Johan Hansen <46825640+vjhansen@users.noreply.github.com>
2019-03-05 16:18:14 +0100

    Update servo_pan_tilt_arduino.ino

--- servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_arduino.ino
@@ -1,58 +1,80 @@
-// 26.feb
-// https://www.instructables.com/id/FACE-TRACKING-USING-ARDUINO-/
-
 #include <Servo.h>
 
-Servo servo_pan, servo_tilt;
-int x, prevX, y, prevY;
+//- https://github.com/embecosm/esp9-arduino-facetracker/blob/master/cam_servo.ino
+
+#define  SERVO_PAN_MAX          180     //- max panning
+#define  SERVO_TILT_MAX         50      //- max tilting (forward)
+#define  HORIZONTAL_PIXELS      1920    //- screen resolution
+#define  VERTICAL_PIXELS        1080    //- screen resolution
+#define  SERVO_PAN_INITIAL_POS  90      //- initial pan-pos
+#define  SERVO_TILT_INITIAL_POS 40      //- initial tilt-pos
+#define  SERVO_PAN_PIN          5
+#define  SERVO_TILT_PIN         6
+#define  baudrate               9600
+#define  incX 1  //- x servo rotation steps (1 deg/step)
+#define  incY 1  //- y servo rotation steps
+
+int prevX = 0; //- previous x-value
+int prevY = 0; //- previous y-value
+
+int Xval = 0;       //- store x data from serial port
+int Yval = 0;       //- store y data from serial port
+float SERVO_PAN_POS  = 0;
+float SERVO_TILT_POS = 0;
+int errorX = 10;  //- significant increments of horizontal (x) camera movement
+int errorY = 10;  //- significant increments of vertical (y) camera movement
+
+Servo SERVO_PAN;
+Servo SERVO_TILT;
 
 void setup() {
-  Serial.begin(9600);
-  servo_pan.attach(10);
-  servo_tilt.attach(9);
-  servo_pan.write(90);
-  servo_tilt.write(40);
+  Serial.begin(baudrate);
+
+  SERVO_TILT.attach(SERVO_TILT_PIN);
+  SERVO_PAN.attach(SERVO_PAN_PIN);
+
+  SERVO_PAN.write(SERVO_PAN_INITIAL_POS);
+  SERVO_TILT.write(SERVO_TILT_INITIAL_POS);
 }
 
-void loop() {
-  if (Serial.available() > 0) 
-  {
-    if (Serial.read() == 'X') 
-    {
-      x = parseInt();
-      Serial.println(x);
-      if (Serial.read() == 'Y') 
-      {
-        y = parseInt();
-        Serial.println(y);
-        moveCam();
+void loop () {
+  while (Serial.available() > 0) {
+    if (Serial.read() == 'X') {
+      Xval = Serial.parseInt();
+      Serial.print(Xval);
+      if (Serial.read() == 'Y') {
+        Yval = Serial.parseInt();
       }
-     }
-     while(Serial.available() > 0) 
-     {
-        Serial.read();
-     }
     }
- }
 
- void moveCam() {
-  if (prevX != x || prevY != y)
-  {
-    prevX = x;
-    prevY = y;
+    //- read last servos positions
+    SERVO_PAN_POS = SERVO_PAN.read();
+    SERVO_TILT_POS = SERVO_TILT.read();
 
-    // map(value, fromLow, fromHigh, toLow, toHigh)
-    // https://www.arduino.cc/reference/en/language/functions/math/map/
-    
-      // må teste her, setter min/max verdier for servoene
-      int servoX = map(x, 600, 0, 70, 179);
-      int servoY = map(y, 450, 0, 179, 95);
-    
-    servoX = min(servoX, 179);
-    servoX = max(servoX, 70);
-    servoY = min(servoY, 180);
-    servoY = max(servoY, 95);
-    servo_pan.write(servoX);
-    servo_tilt.write(servoY);
+    if (prevX != Xval || prevY != Yval) {
+      prevX = Xval;
+      prevY = Yval;
+
+      //- if X-coord is to the left from middle
+      if (Xval < (HORIZONTAL_PIXELS / 2 - errorX)) {
+        if (SERVO_PAN_POS >= errorX) SERVO_PAN_POS += incX; //- move to the left.
+      }
+      //- if X-coord is to the right from the middle
+      else if (Xval > (HORIZONTAL_PIXELS / 2 + errorX)) {
+        if (SERVO_PAN_POS <= SERVO_PAN_MAX - errorX) SERVO_PAN_POS -= incX; //- move to the right.
+      }
+
+      //- if Y-coord is below midscreen
+      if (Yval < (VERTICAL_PIXELS / 2 - errorY)) {
+        if (SERVO_TILT_POS >= 20) SERVO_TILT_POS += incY;   //- tilt down
+      }
+
+      //- if Y-coord is above midscreen
+      else if (Yval > (VERTICAL_PIXELS / 2 + errorY)) {
+        if (SERVO_TILT_POS <= 50) SERVO_TILT_POS -= incY;   //-  tilt up
+      }
+      SERVO_PAN.write(SERVO_PAN_POS);
+      SERVO_TILT.write(SERVO_TILT_POS);
+    }
   }
 }
