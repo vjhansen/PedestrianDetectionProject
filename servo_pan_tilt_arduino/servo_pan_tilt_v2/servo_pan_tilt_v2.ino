ab0ca6f1c1434b00d313c8b0c60a13832260f15b
katostrekka <47088942+katostrekka@users.noreply.github.com>
2019-04-02 13:54:31 +0200

    Add files via upload

--- servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
@@ -4,8 +4,9 @@
 #define   SERVO_TILT_PIN       6
 #define   SERVO_PAN_INIT_POS   90
 #define   SERVO_TILT_INIT_POS  5
-#define   SERVO_TILT_MAX       25
+#define   SERVO_TILT_MAX       20
 #define   SERVO_PAN_MAX        180
+#define   SERVO_MIN            0
 
 
 
@@ -35,9 +36,9 @@ void loop() {
      x = 1;
      delay(5000);
 
-     //kjør dette en gang, bruk funksjoner til å gi instrukser
-     if(y <=  0){
-      opp(8, 100);
+     //kjør dette to ganger, bruk funksjoner til å gi instrukser til servoer
+     if(y <=  1){
+      opp(5, 100);
       venstre(150, 100);
       delay(5000);
       hoyre(30, 100);
@@ -48,22 +49,26 @@ void loop() {
 
 //funksjon for å øke posisjon til servo, skal samsvare med å
 //vri til venstre. Tar inn to variabler, første for hvor langt
-//vi skal vri, den andre for hvor fort (større gir saktere
-//pga delay blir større)
+//vi skal vri, den andre for hvor fort (større verdi gir saktere
+//hastighet pga delay blir større)
 //Lager tre til bare med høyre, opp og ned, samme type input.
 
 int venstre(int a, int b){
-  for(int i = SERVO_PAN.read(); i <= a; i = i + 1){
-     SERVO_PAN.write(i);
-     delay(b);
+  if(a <= SERVO_PAN_MAX){
+    for(int i = SERVO_PAN.read(); i <= a; i = i + 1){
+       SERVO_PAN.write(i);
+       delay(b);
+    }
   }
 }
 
 int hoyre(int c, int d){
-  for(i = SERVO_PAN.read(); i >= c; i = i - x){
-     SERVO_PAN.write(i);
-     delay(d);
-   }
+  if(c >= SERVO_MIN){
+    for(i = SERVO_PAN.read(); i >= c; i = i - x){
+       SERVO_PAN.write(i);
+       delay(d);
+    }
+  }
 }
 
 int opp(int e, int f){
@@ -76,8 +81,10 @@ int opp(int e, int f){
 }
 
 int ned(int g, int h){
-  for(j = SERVO_TILT.read(); j >= g; j = j - x){
-     SERVO_TILT.write(j);
-     delay(h);
+  if(g >= SERVO_MIN){
+    for(j = SERVO_TILT.read(); j >= g; j = j - x){
+       SERVO_TILT.write(j);
+       delay(h);
+    }
   }
 }
