135470d7157d4c47f7ccf72050abca0214e2d854
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-04-07 16:50:28 +0200

    Update servo_pan_tilt_v2.ino

--- servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
@@ -1,3 +1,4 @@
+// Skanneprogram
 #include <Servo.h>
 
 #define   SERVO_PAN_PIN        5
@@ -7,8 +8,7 @@
 #define   SERVO_TILT_MAX       20
 #define   SERVO_PAN_MAX        180
 #define   SERVO_MIN            0
-
-#define   baudrate        9600
+#define   baudrate             9600
 
 //definerer variabler
 Servo SERVO_PAN;
@@ -31,7 +31,7 @@ void loop() {
      delay(5000);
 
      //kjør dette to ganger, bruk funksjoner til å gi instrukser til servoer
-     if(y <=  1){
+     if(y <=  1) {
       opp(5, 100);
       venstre(150, 100);
       delay(5000);
@@ -47,8 +47,8 @@ void loop() {
 //hastighet pga delay blir større)
 //Lager tre til bare med høyre, opp og ned, samme type input.
 
-int venstre(int a, int b){
-  if(a <= SERVO_PAN_MAX){
+int venstre(int a, int b) {
+  if(a <= SERVO_PAN_MAX) {
     for(int i = SERVO_PAN.read(); i <= a; i = i + 1){
        SERVO_PAN.write(i);
        delay(b);
@@ -56,8 +56,8 @@ int venstre(int a, int b){
   }
 }
 
-int hoyre(int c, int d){
-  if(c >= SERVO_MIN){
+int hoyre(int c, int d) {
+  if(c >= SERVO_MIN) {
     for(i = SERVO_PAN.read(); i >= c; i = i - x){
        SERVO_PAN.write(i);
        delay(d);
@@ -65,8 +65,8 @@ int hoyre(int c, int d){
   }
 }
 
-int opp(int e, int f){
-  if(e <= SERVO_TILT_MAX){
+int opp(int e, int f) {
+  if(e <= SERVO_TILT_MAX) {
     for(j = SERVO_TILT.read(); j <= e; j = j + x){
       SERVO_TILT.write(j);
       delay(f);
@@ -74,8 +74,8 @@ int opp(int e, int f){
   }
 }
 
-int ned(int g, int h){
-  if(g >= SERVO_MIN){
+int ned(int g, int h) {
+  if(g >= SERVO_MIN) {
     for(j = SERVO_TILT.read(); j >= g; j = j - x){
        SERVO_TILT.write(j);
        delay(h);
