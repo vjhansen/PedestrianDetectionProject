46b9cad30cfb08dcf79ecb0fd97c9b4919f8d386
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-04-23 21:45:20 +0200

    Update augment-dataset.py

--- augment-dataset.py
+++ augment-dataset.py
@@ -4,9 +4,9 @@ import os, sys, cv2, numpy
 
 img_num = 1
 dir_src = "Datasett/"
-dir_dst = "aug_datasett/"
 src_files = os.listdir(dir_src)
 
+dir_dst = "agmntd_datasett/"
 if not os.path.exists(dir_dst):
 	os.makedirs(dir_dst)
 
