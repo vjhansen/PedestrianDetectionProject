6d1191ea00c726db746ac72a1d455b8209bba439
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-05-09 19:14:04 +0200

    Update styre_servo_keys.ino

--- styre_servo_keys/styre_servo_keys.ino
+++ styre_servo_keys/styre_servo_keys.ino
@@ -1,19 +1,29 @@
 // PDP
 // Styre servo-rigg med tastatur
 
-#include<Servo.h>
+// test om den fungerer!
+
+#include <Servo.h>
+
+#define   SERVO_PAN_PIN        5
+#define   SERVO_TILT_PIN       6
+#define   baudrate             9600
 
 Servo servo_pan;
 Servo servo_tilt;
+
 int pos_pan = 0;
 int pos_tilt = 50;
 int val;
+int wait_time = 15;
+int pan_inc = 2;
+int tilt_inc = 1;
 
 void setup() {
-  Serial.begin(9600);
-  servo_pan.attach(5);
+  Serial.begin(baudrate);
+  servo_pan.attach(SERVO_PAN_PIN);
   servo_pan.write(pos_pan);
-  servo_tilt.attach(6);
+  servo_tilt.attach(SERVO_TILT_PIN);
   servo_tilt.write(pos_tilt);
 }
 
@@ -21,25 +31,28 @@ void loop() {
   if (Serial.available()) {
     val = Serial.read();
     
-    if (val == 'd') {   // D = høyre
-      pos_pan -= 2;
+    if (val == 'd') {   // d = høyre
+      pos_pan -= pan_inc;
       servo_pan.write(pos_pan);
-      delay(15);
+      delay(wait_time);
     }
-    if (val == 'a') {   // A = venstre
-      pos_pan += 2;
+    
+    if (val == 'a') {   // a = venstre
+      pos_pan += pan_inc;
       servo_pan.write(pos_pan); 
-      delay(15);
+      delay(wait_time);
     }
-    if (val == 'w') {   // W = opp
-      pos_tilt += 1;
+    
+    if (val == 'w') {   // w = opp
+      pos_tilt += tilt_inc;
       servo_tilt.write(pos_tilt);
-      delay(15);
+      delay(wait_time);
     }
-    if (val == 's') {   // S = ned
-      pos_tilt -= 1;
+    
+    if (val == 's') {   // s = ned
+      pos_tilt -= tilt_inc;
       servo_tilt.write(pos_tilt);
-      delay(15);
+      delay(wait_time);
     }
   }
 }
