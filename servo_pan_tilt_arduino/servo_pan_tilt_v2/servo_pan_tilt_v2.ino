9769ae61443efcb7f532edae0b0c7b631dc01f60
Victor J. Hansen <46825640+vjhansen@users.noreply.github.com>
2019-05-09 19:03:10 +0200

    Update servo_pan_tilt_v2.ino

--- servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
+++ servo_pan_tilt_arduino/servo_pan_tilt_v2/servo_pan_tilt_v2.ino
@@ -14,74 +14,74 @@
 // - definerer variabler
 Servo SERVO_PAN;
 Servo SERVO_TILT;
-int tid = 0;
-int y = 0;
-int i, x, j;
+int time = 0;
+int current_round = 0; // antall runder
+int inc = 0;
+int num_rounds = 0;
 
 void setup() {
   Serial.begin(baudrate);
   SERVO_PAN.attach(SERVO_PAN_PIN);
   SERVO_TILT.attach(SERVO_TILT_PIN);
-  SERVO_PAN.write(0);
+  
+  SERVO_PAN.write(0); //hvorfor 0?
   SERVO_TILT.write(0);
 }
 
 void loop() {
-     x = 1;
-     delay(5000);
-
-     // - kjør dette to ganger, bruk funksjoner til å gi instrukser til servoer
-     if(y <=  1) {
-      opp(5, 100);
-      venstre(150, 100);
-      delay(5000);
-      hoyre(30, 100);
-      y = y + 1;
-     }        
+  inc = 1;  // inkrementering
+  num_rounds = 1;
+  delay(5000);
+  
+  // - kjør dette to ganger, bruk funksjoner til å gi instrukser til servoer
+  if(current_round <=  num_rounds) {
+    up(5, 100);
+    left(150, 100);
+    delay(5000);
+    right(30, 100);
+    current_round++;
+  }        
 }
-
 
 /*
-      funksjon for å øke posisjon til servo, skal samsvare med å
-      vri til venstre. Tar inn to variabler, første for hvor langt
-      vi skal vri, den andre for hvor fort (større verdi gir saktere
-      hastighet pga delay blir større
+      'up, left og right' er funksjoner for å øke posisjon til servo.
+      De tar inn to variabler:
+      - hvor langt servoene skal flytte seg (ex: dist_up)
+      - delay, hvor fort servoene skal flytte seg (ex: delay_up, stort delay gir saktere hastighet)
 */
 
-//Lager tre til bare med høyre, opp og ned, samme type input.
-
-int venstre(int a, int b) {
-  if(a <= SERVO_PAN_MAX) {
-    for(int i = SERVO_PAN.read(); i <= a; i = i + 1){
-       SERVO_PAN.write(i);
-       delay(b);
+int left(int dist_left, int delay_left) {
+  if(dist_left <= SERVO_PAN_MAX) {
+    for(int pos_left = SERVO_PAN.read(); pos_left <= dist_left; pos_left = pos_left + inc) {
+       SERVO_PAN.write(pos_left);
+       delay(delay_left);
     }
   }
 }
 
-int hoyre(int c, int d) {
-  if(c >= SERVO_MIN) {
-    for(i = SERVO_PAN.read(); i >= c; i = i - x){
-       SERVO_PAN.write(i);
-       delay(d);
+int right(int dist_right, int delay_right) {
+  if(dist_right >= SERVO_MIN) {
+    for(int pos_right = SERVO_PAN.read(); pos_right >= dist_right; pos_right = pos_right - inc) {
+       SERVO_PAN.write(pos_right);
+       delay(delay_right);
     }
   }
 }
 
-int opp(int e, int f) {
-  if(e <= SERVO_TILT_MAX) {
-    for(j = SERVO_TILT.read(); j <= e; j = j + x){
-      SERVO_TILT.write(j);
-      delay(f);
+int up(int dist_up, int delay_up) {
+  if(dist_up <= SERVO_TILT_MAX) {
+    for(int pos_up = SERVO_TILT.read(); pos_up <= dist_up; pos_up = pos_up + inc){
+      SERVO_TILT.write(pos_up);
+      delay(delay_up);
     }
   }
 }
 
-int ned(int g, int h) {
-  if(g >= SERVO_MIN) {
-    for(j = SERVO_TILT.read(); j >= g; j = j - x){
-       SERVO_TILT.write(j);
-       delay(h);
+int down(int dist_down, int delay_down) {
+  if(dist_down >= SERVO_MIN) {
+    for(int pos_down = SERVO_TILT.read(); pos_down >= dist_down; pos_down = pos_down - inc){
+       SERVO_TILT.write(pos_down);
+       delay(delay_down);
     }
   }
 }
