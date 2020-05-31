777cabb06b5a11b7ea2dbcb3e0806153ac8adbaf
Victor Hansen <46825640+vjhansen@users.noreply.github.com>
2020-05-31 20:34:36 +0200

    Update dl-img-url.py

--- dl-img-url.py
+++ dl-img-url.py
@@ -1,11 +1,11 @@
-# PDP
 # Used for downloading huge amounts of images from ImageNet via url.
 
 import os, cv2
 import urllib.request as urlreq
 import numpy as np
 
-img_urls = urlreq.urlopen('http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n09629752').read().decode()
+# the URL below is probably outdated, its added as an example.
+img_urls = urlreq.urlopen('http://www.image-net.org/api/text/imagenet.synset.geturls?wnid=n09629752').read().decode() 
 img_num = 1
 if not os.path.exists('datasett'):
     os.makedirs('datasett')
@@ -13,7 +13,7 @@ for i in img_urls.split('\n'):
     try:
         print(i)
         urlreq.urlretrieve(i, "datasett/"+str(img_num)+".jpg")
-        img = cv2.imread("datasett/"+str(img_num)+".jpg") # bruk IMREAD_GRAYSCALE for å gjøre bildene svart/hvit
+        img = cv2.imread("datasett/"+str(img_num)+".jpg") # use IMREAD_GRAYSCALE to make images black/white
         cv2.imwrite("datasett/"+str(img_num)+".jpg",img)
         print(img_num)
         img_num += 1
